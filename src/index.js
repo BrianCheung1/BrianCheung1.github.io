bf0c6576953ea46dc9cd17679f44950d84c7c803
Brian Cheung <44447718+BrianCheung1@users.noreply.github.com>
2021-10-24 03:53:19 -0400

    fix for website link

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </HashRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
